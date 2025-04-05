934c641e230834ad29eca3ca9701a9739bb2fd97
stvy <stvycoding@gmail.com>
2025-04-05 14:53:04 +0530

    added typewriter effect

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <footer className="w-full bg-stone-100 text-zinc-600 py-10 px-4">
+            <div className="max-w-6xl mx-auto flex flex-col items-center">
+                {/* Navigation links */}
+                <nav className="grid grid-flow-col gap-6 mb-6">
+                    <Link to="#about" className="hover:text-blue-500 transition-colors">About us</Link>
+                    <Link to="#faq" className="hover:text-blue-500 transition-colors">FAQs</Link>
+                    <a href="https://github.com/Priyesh1311421/SideQuest"
+                        className="hover:text-blue-500 transition-colors"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        Github
+                    </a>
+                </nav>
+                {/* Copyright text */}
+                <p className="text-sm">Made with ❤️ in 24 hours</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { TypewriterEffectSmooth } from "../components/Typewritter-effect";
 
 const LandingPage = () => {
+
+    const words = [
+        {
+          text: "Where ",
+        },
+        {
+          text: "cultures ",
+        },
+        {
+          text: "meet, ",
+        },
+        {
+          text: "stories ",
+        },
+        {
+          text: "begin.",
+        },
+      ];
+
     return(
         <div className="bg-stone-200 flex flex-col items-center">
             <Header/>
+            <div className="min-h-screen mt-50">
+                <TypewriterEffectSmooth words={words}/>
+            </div>
+            <Footer/>
         </div>
     )
 }
